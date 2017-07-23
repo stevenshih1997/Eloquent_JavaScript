e5d444916645d26b2e2bdab07ccec7f8d10fb2ea
Steven Shih <stevenshih1997@gmail.com>
2017-07-23 00:00:30 -0700

    update

--- chapter9.js
+++ chapter9.js
@@ -46,3 +46,22 @@ console.log(cartoonCrying.test("Boohoooohoohooo"));
 console.log('one two 100'.match(/\d+/));
 // ['100']
 
+
+// /abc/ A sequence of characters
+// /[abc]/ Any character from a set of characters
+// /[^abc]/  Any character not in a set of characters
+// /[0-9]/ Any character in a range of characters
+// /x+/  One or more occurrences of the pattern x
+// /x+?/ One or more occurrences, nongreedy
+// /x*/  Zero or more occurrences
+// /x?/  Zero or one occurrence
+// /x{2,4}/  Between two and four occurrences
+// /(abc)/ A group
+// /a|b|c/ Any one of several patterns
+// /\d/  Any digit character
+// /\w/  An alphanumeric character (“word character”)
+// /\s/  Any whitespace character
+// /./ Any character except newlines
+// /\b/  A word boundary
+// /^/ Start of input
+// /$/ End of input
